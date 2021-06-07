1c3b90dd618e7ce35d82c00a8e51bcbb9722abda
Raphaël Lizot <raphael.lizot@gmail.com>
2021-06-07 17:13:51 +0200

    Correction des fichiers

--- Code/metier/Joueur.java
+++ Code/metier/Joueur.java
@@ -7,31 +7,44 @@ import java.util.ArrayList;
 
 public class Joueur 
 {
-    private ArrayList<Dalle>   tabDalles;
-    private ArrayList<Pilier>  tabPilier;
+    private ArrayList<Dalle>   listeDalles;
+    private ArrayList<Pilier>  listePilier;
 
-    private int  nbPilier;
     private int  numJoueur ;
     private char coulJoueur;
+    private int  nbPilier;
+    private int  nbPilierDetruis;
 
     public Joueur(int numJoueur, char coulJoueur)
     {
-        this.numJoueur = numJoueur;
-        this.coulJoueur = coulJoueur;
-        this.nbPilier = 24;
+        this.numJoueur       = numJoueur;
+        this.coulJoueur      = coulJoueur;
+        this.nbPilier        = 24;
+        this.nbPilierDetruis = 0;
 
     }
 
-    public void poserPilier()
+    public boolean poserPilier()
     {
         if(nbPilier > 0)
         {
             this.nbPilier--;
+            return true;
         }
+
+        return false;
     }
 
     public void ajouterDalles(Dalle dalle)
     {
-        this.tabDalles.add(dalle);
+        this.listeDalles.add(dalle);
     }
+
+    public int  getNumJoueur()       { return this.numJoueur;       }
+    public int  getNbPilier ()       { return this.nbPilier;        }
+    public int  getNbPilierDetruis() { return this.nbPilierDetruis; }
+    public char getCouleur()         { return this.coulJoueur;      }
+
+    public ArrayList<Dalle>  getListeDalles() { return this.listeDalles; }
+    public ArrayList<Pilier> getListePilier() { return this.listePilier; }
 }
