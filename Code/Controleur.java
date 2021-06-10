64bb47a10407e80d5c7ac54465838e6d37298f43
Raphaël Lizot <raphael.lizot@gmail.com>
2021-06-10 09:29:20 +0200

    Bug controleur

--- Code/Controleur.java
+++ Code/Controleur.java
@@ -44,7 +44,7 @@ public class Controleur
 			//Choix de jeux du Joueur 1
 
 			System.out.println("Tour du joueur 1 (" + this.metier.getJoueur(1).getCouleur() + ") :");
-			do
+			while(!bOk)
 			{
 				System.out.print("\tChoissisez une dalle : ");
 				nomDalle = Character.toUpperCase(Clavier.lire_char());
@@ -63,9 +63,10 @@ public class Controleur
 					System.out.print("\tQuel côte (0 à 5): ");
 					numSommet= Clavier.lire_int();
 				}
+
 				bOk = this.metier.poserPilier(1, nomDalle, numSommet);
 			}
-			while(!bOk);
+			
 
 			//On met à jour la Grille
 	
@@ -76,7 +77,8 @@ public class Controleur
 			//Choix de jeux du Joueur 2
 
 			System.out.println("Tour du joueur 2 (" + this.metier.getJoueur(2).getCouleur() + ") :");
-			do
+			bOk = false;
+			while(!bOk)
 			{	
 				System.out.print("\tChoissisez une dalle : ");
 				nomDalle = Character.toUpperCase(Clavier.lire_char());
@@ -96,8 +98,7 @@ public class Controleur
 					numSommet= Clavier.lire_int();
 				}
 				bOk = this.metier.poserPilier(2, nomDalle, numSommet);	
-			}
-			while(!bOk);	
+			}	
 
 			this.ihm.miseAJourGrille();
 			this.metier.finDeTour();
