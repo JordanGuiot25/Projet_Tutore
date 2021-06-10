c99a78714c244932463ee849d267ecde9c8f086e
Gaspard Gordien <gaspard.gordien@etu.univ-lehavre.fr>
2021-06-10 15:06:44 +0200

    Menu mieux et moins moche

--- Code/ihm/gui/PanelMenu.java
+++ Code/ihm/gui/PanelMenu.java
@@ -4,8 +4,11 @@ import PilierDeLaTerre.Controleur;
 import PilierDeLaTerre.metier.Dalle;
 import PilierDeLaTerre.metier.Pilier;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -13,52 +16,113 @@ import java.awt.Image;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.*;
-import java.awt.image.*;
+import java.awt.Dimension;
 
 import java.util.ArrayList;
 
 public class PanelMenu extends JPanel implements ActionListener
 {
-	private static final Image RESSOURCE_DALLE        = java.awt.Toolkit.getDefaultToolkit().getImage("../Ressources/Dalle.png"       );
-	private static final Image RESSOURCE_ANNEAU_MARON = java.awt.Toolkit.getDefaultToolkit().getImage("../Ressources/anneau_maron.png");
-	private static final Image RESSOURCE_ANNEAU_GRIS  = java.awt.Toolkit.getDefaultToolkit().getImage("../Ressources/anneau_gris.png" );
 	private Controleur  ctrl;
-    private JButton bouttonPartieRapide;
-    private JButton bouttonPartieCustom;
-    private Image      imgFond;
+
+    private JButton btnPartieRapide     ;
+    private JButton btnLancerScenar     ;
+    private JButton btnChargerSauvegarde;
+    private JButton btnPartieCustom     ;
+    private JButton btnQuitter          ;
+    private Image   imgFond             ;
+    private Image   imgPartieRapide     ;
+    private Image   imgLancerScenario   ;
+    private Image   imgChargerSave      ;
+    private Image   imgPartieCustom     ;
+    private Image   imgQuitter          ;
 
 	public PanelMenu(Controleur  ctrl)
 	{
         /*creation des composants */
-		this.setLayout(new BorderLayout());
-        this.ctrl  = ctrl;
-        this.bouttonPartieCustom = new JButton("Partie custom");
-        this.bouttonPartieRapide = new JButton("Partie rapide");
-        JPanel panelBouton = new JPanel(new GridLayout(1,2));
-        this.imgFond = getToolkit().getImage ( "../Ressources/fond.png" );
+		this.setLayout(new GridLayout(6,3,20,20));
+        this.ctrl                 = ctrl;
+        this.imgFond              = getToolkit().getImage ( "../Ressources/fond.png" );
+        this.imgPartieRapide      = getToolkit().getImage ( "../Ressources/PartieRapide.png" );
+        this.imgLancerScenario    = getToolkit().getImage ( "../Ressources/LancerScenario.png" );
+        this.imgChargerSave       = getToolkit().getImage ( "../Ressources/ChargerSauvegarde.png" );
+        this.imgPartieCustom      = getToolkit().getImage ( "../Ressources/PartieCustom.png" );
+        this.imgQuitter           = getToolkit().getImage ( "../Ressources/Quitter.png" );
+        
+        /*boutons*/
+        this.btnPartieRapide      = new JButton(new ImageIcon(imgPartieRapide));
+        this.btnLancerScenar      = new JButton(new ImageIcon(imgLancerScenario));
+        this.btnChargerSauvegarde = new JButton(new ImageIcon(imgChargerSave));
+        this.btnPartieCustom      = new JButton(new ImageIcon(imgPartieCustom));
+        this.btnQuitter           = new JButton(new ImageIcon(imgQuitter));
+
+        //taille
+        this.btnPartieRapide.setPreferredSize(new Dimension(50,50));
+        this.btnLancerScenar.setPreferredSize(new Dimension(50,50));
+        this.btnChargerSauvegarde.setPreferredSize(new Dimension(50,50));
+        this.btnPartieCustom.setPreferredSize(new Dimension(50,50));
+        this.btnQuitter.setPreferredSize(new Dimension(50,50));
+
+        //transparence
+        this.btnPartieRapide.setOpaque(false);
+        this.btnPartieRapide.setContentAreaFilled(false);
+        this.btnPartieRapide.setBorderPainted(false);
+        this.btnLancerScenar.setOpaque(false);
+        this.btnLancerScenar.setContentAreaFilled(false);
+        this.btnLancerScenar.setBorderPainted(false);
+        this.btnChargerSauvegarde.setOpaque(false);
+        this.btnChargerSauvegarde.setContentAreaFilled(false);
+        this.btnChargerSauvegarde.setBorderPainted(false);
+        this.btnPartieCustom.setOpaque(false);
+        this.btnPartieCustom.setContentAreaFilled(false);
+        this.btnPartieCustom.setBorderPainted(false);
+        this.btnQuitter.setOpaque(false);
+        this.btnQuitter.setContentAreaFilled(false);
+        this.btnQuitter.setBorderPainted(false);
+
+
         /* placement des composants */
-        this.add(panelBouton, BorderLayout.SOUTH);
-        panelBouton.add(bouttonPartieRapide);
-        panelBouton.add(bouttonPartieCustom);
-        panelBouton.setOpaque ( true );
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(this.btnPartieRapide);
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(this.btnPartieCustom);
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(this.btnLancerScenar);
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));//this.add(this.btnChargerSauvegarde);
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+        this.add(this.btnQuitter);
+        this.add(new JLabel(""));
+        this.add(new JLabel(""));
+
 
         /* activation des composants */
-        this.bouttonPartieCustom.addActionListener(this);
-        this.bouttonPartieRapide.addActionListener(this);
+        this.btnPartieRapide.addActionListener(this);
+        this.btnLancerScenar.addActionListener(this);
+        this.btnChargerSauvegarde.addActionListener(this);
+        this.btnPartieCustom.addActionListener(this);
+        this.btnQuitter.addActionListener(this);
         repaint();
 
     }
 
 	public void actionPerformed(ActionEvent e)
     {
-        if (e.getSource() == this.bouttonPartieCustom)
-        {
-            ctrl.LancerPartieCustom();
-        }
-        if (e.getSource() == this.bouttonPartieRapide)
-        {
+        if(e.getSource() == this.btnPartieRapide)
             ctrl.LancerPartieRapide();
-        }
+        else if(e.getSource() ==this.btnLancerScenar) 
+            ctrl.ouvrirScenar();
+        else if(e.getSource() == this.btnChargerSauvegarde)
+            ctrl.LancerSauv();
+        else if(e.getSource() ==this.btnPartieCustom)
+            ctrl.LancerEditeur();
+        else if(e.getSource() ==this.btnQuitter)
+            ctrl.quitter();                
     }
 
     public void paintComponent(Graphics g)
