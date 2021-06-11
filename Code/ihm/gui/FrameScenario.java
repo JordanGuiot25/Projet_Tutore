8014b6fa10b1c007fe2a2063a20ecd7de6787f7d
Jordan Guiot <Jordan.guiot25@gmail.com>
2021-06-11 16:37:25 +0200

    Finalisation de la frame des scenario

--- Code/ihm/gui/FrameScenario.java
+++ Code/ihm/gui/FrameScenario.java
@@ -1,6 +1,7 @@
 package PilierDeLaTerre.ihm.gui;
 
 import PilierDeLaTerre.Controleur;
+import PilierDeLaTerre.ihm.gui.PanelScenario;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
@@ -18,76 +19,28 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 
-public class FrameScenario extends JFrame implements ActionListener
+public class FrameScenario extends JFrame
 {
     private Controleur ctrl;
-    private JComboBox <String> cboxScenario;
-    private String[] tabString;
-    private static int NB_SCENARIO = 6;
-    private JButton btnJouer;
-    private JPanel panelBox;
-    private Image  imgFond;
 
-    
+    private PanelScenario panelScenario;
+
     public FrameScenario(Controleur ctrl)
     {
         this.ctrl = ctrl;
 
-        this.imgFond              = getToolkit().getImage ( "../Ressources/imgScenario.jpg" );
-        this.repaint();
-        this.tabString = new String [this.NB_SCENARIO];
-
-        for(int i = 0 ; i < 6 ; i++)
-        {
-            this.tabString[i] = "Scénario " + i;
-        }
-
-		this.setTitle    ("Scenario");
-        this.setLayout( new BorderLayout());
-        this.setSize     (400, 350    );
+        this.setTitle    ("Scenario");
+        this.setSize     (560, 350    );
         this.setLocation (400, 0    );
-        this.cboxScenario = new JComboBox<String>(  this.tabString );
-        this.btnJouer = new JButton("Jouer");
-        this.panelBox = new JPanel();
-     
-       
-       
-        this.panelBox.add(new JLabel("Scénario à lancer :"), BorderLayout.NORTH);
-        this.panelBox.add(this.cboxScenario);
-        this.add(this.panelBox,BorderLayout.CENTER);
-        this.add(this.btnJouer,BorderLayout.SOUTH);
 
-        this.cboxScenario.addActionListener(this);
-        this.btnJouer.addActionListener(this);
+        //Creation des composants
+        this.panelScenario = new PanelScenario(ctrl);
+
+        //Positionnement des composants
+        this.add(this.panelScenario);
 
         
         this.setVisible(false);
         this.setResizable(false);
-
-        
     }
-
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource() == this.btnJouer)
-        {
-            String sText = ""+this.cboxScenario.getSelectedItem();
-            int numTmp = Integer.parseInt(sText.substring(9));
-            this.ctrl.setScenario(numTmp);
-
-            this.ctrl.lancerScenario();
-        }
-    }
-    public void paint(Graphics g)
-    {
-        super.paint(g);
-
-        Graphics2D g2 = (Graphics2D) g;
-
-        g2.drawImage ( this.imgFond, 0, 0, this );
-
-        System.out.println("coucou");
-    }
-   
-
 }
