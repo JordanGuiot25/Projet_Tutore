e849d34881df6c77234c60d0f255e08117747b53
LordDuchess <79205189+LordDuchess@users.noreply.github.com>
2021-06-11 18:06:19 +0200

    Pour le commit precedent: fusion de l'application principal avec l'editeur de parterre. Pour ce commit: Ajout de l'image semi transparante des des dalles
    PS : Est-ce normal qu'on ne peut pas aller dans Lancer Scenario ?

--- Code/ihm/gui/PanelMenu.java
+++ Code/ihm/gui/PanelMenu.java
@@ -110,6 +110,7 @@ public class PanelMenu extends JPanel implements ActionListener
 
 
         /* activation des composants */
+		/**/
         this.btnPartieRapide     .addActionListener(this);
         this.btnLancerScenar     .addActionListener(this);
         this.btnChargerSauvegarde.addActionListener(this);
