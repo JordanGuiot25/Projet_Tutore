fcda769eba78868624d6882723a4e832ca838840
Jordan Guiot <Jordan.guiot25@gmail.com>
2021-06-14 13:52:14 +0200

    java doc frame et panel Scenario

--- Code/ihm/gui/FrameScenario.java
+++ Code/ihm/gui/FrameScenario.java
@@ -1,7 +1,7 @@
-package PilierDeLaTerre.ihm.gui;
+package Equipe_22.ihm.gui;
 
-import PilierDeLaTerre.Controleur;
-import PilierDeLaTerre.ihm.gui.PanelScenario;
+import Equipe_22.Controleur;
+import Equipe_22.ihm.gui.PanelScenario;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
@@ -18,13 +18,18 @@ import java.awt.BorderLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-
+/**La frame scénario
+ * @author Jordan Guiot, Enguerrand Beltran, Raphael Lizot, Gaspard Gordien
+ */
 public class FrameScenario extends JFrame
 {
     private Controleur ctrl;
 
     private PanelScenario panelScenario;
 
+    /**Constructeur
+     * @param ctrl {@link Constructeur}
+     */
     public FrameScenario(Controleur ctrl)
     {
         this.ctrl = ctrl;
@@ -33,14 +38,19 @@ public class FrameScenario extends JFrame
         this.setSize     (560, 350    );
         this.setLocation (400, 0    );
 
-        //Creation des composants
+        /**Creation des composants */
         this.panelScenario = new PanelScenario(ctrl);
 
-        //Positionnement des composants
+        /**Positionnement des composants */
         this.add(this.panelScenario);
 
-        
+        /**On setVisible(false) car quand on l'initialise, on ne veut pas l'afficher directement,
+         * on l'affichera lorsque le joueur clique sur "lancer Scenario" 
+         * et setResizable(false) pour que l'utilisateur ne puisse pas modifier la taille de la fenêtre*/ 
         this.setVisible(false);
         this.setResizable(false);
+        /**Permettre la fermeture du programme quand la fenêtre se ferme */
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
     }
 }
--- Code/ihm/gui/PanelScenario.java
+++ Code/ihm/gui/PanelScenario.java
@@ -1,7 +1,7 @@
-package PilierDeLaTerre.ihm.gui;
+package Equipe_22.ihm.gui;
 
-import PilierDeLaTerre.ihm.gui.FrameScenario;
-import PilierDeLaTerre.Controleur;
+import Equipe_22.ihm.gui.FrameScenario;
+import Equipe_22.Controleur;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -19,40 +19,55 @@ import java.awt.BorderLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-
+/**Le panel des scénarios
+ * @author Jordan Guiot, Enguerrand Beltran, Raphael Lizot, Gaspard Gordien
+ */
 public class PanelScenario extends JPanel implements ActionListener
 {
-	private static int NB_SCENARIO = 6;
-
+	/**Nombres de scénario max à changer si on veut mettre plus de scénario */
+	private static int NB_SCENARIO = 6;	
 	private Controleur ctrl;
 
 	private JComboBox<String> cboxScenario;
 	private JButton           btnJouer;
 	private JButton			  btnRetour;
-	private Image  imgFond;
+	private Image  			  imgFond;
 
+
+	/**Constructeur
+	 * @param ctrl {@link Controleur}
+	 **/	
 	public PanelScenario(Controleur ctrl)
 	{
 		this.ctrl = ctrl;
 		this.setLayout   ( new BorderLayout() );
 		this.setOpaque(false);
+		
+		/**image de fond dans le PanelScenario */
+		this.imgFond = java.awt.Toolkit.getDefaultToolkit().getImage( "../Ressources/fond.png" );	
 
-		this.imgFond = java.awt.Toolkit.getDefaultToolkit().getImage( "../Ressources/fond.png" );
+		/**tableau de String qui va stocker les scénarios avec leur numéro pour le mettre dans un JComboBox	 */
+		String[] tabString = new String [this.NB_SCENARIO];	
 
-		String[] tabString = new String [this.NB_SCENARIO];
-
-		for(int i = 0 ; i < 6 ; i++)
+		/**Initialisation du tableau avec les Scénarios */
+		for(int i = 0 ; i < 6 ; i++)																
 			tabString[i] = "Scénario " + i;
 
-		//Creation des composants
+		/**Creation des composants */
 		this.cboxScenario = new JComboBox<String>(  tabString );
-		this.btnJouer     = new JButton("Jouer");
-		this.btnRetour	  = new JButton("Retour au menu ");
+
+		/**Bouton qui va servir pour lancer le scénario qui a été sélectionné */
+		this.btnJouer     = new JButton("Jouer");		
+
+		/**Bouton servant à retourner au menu principale lorsqu'on était dans le lanceur de scénario */
+		this.btnRetour	  = new JButton("Retour au menu ");										
 		JPanel panelBox   = new JPanel();
-		panelBox.setOpaque(true);
+		
+		/**Pour permettre de voir l'image de fond */
+		panelBox.setOpaque(true);																	
      
-	 	//Positionnement des composants
-		panelBox.add(this.btnRetour);
+	 	/**Positionnement des composants */
+		panelBox.add(this.btnRetour;
 		panelBox.add(new JLabel("Scénario à lancer :"));
 		panelBox.add(this.cboxScenario);
 		
@@ -61,7 +76,7 @@ public class PanelScenario extends JPanel implements ActionListener
 		this.add( this.btnJouer,BorderLayout.SOUTH  );
 
 
-		//Activation des compossants
+		/**Activation des compossants */
 		this.cboxScenario.addActionListener(this);
 		this.btnJouer.addActionListener(this);
 		this.btnRetour.addActionListener(this);
@@ -69,24 +84,30 @@ public class PanelScenario extends JPanel implements ActionListener
 		repaint();
 	}
 
-
-	public void actionPerformed(ActionEvent e)
+	/**Detecte si les boutons sont appuyés */
+	public void actionPerformed(ActionEvent e)	
 	{
-		if(e.getSource() == this.btnJouer)
+		/**Cette condition sert pour lancer le scénario après avoir appuyé sur le bouton jouer*/
+		if(e.getSource() == this.btnJouer)							
 		{
-			String sText = ""+this.cboxScenario.getSelectedItem();
-			int numTmp = Integer.parseInt(sText.substring(9));
+			/**Récupération du numéro de scénario */
+			String sText = ""+this.cboxScenario.getSelectedItem();	
+			int numTmp = Integer.parseInt(sText.substring(9));		
 			this.ctrl.setScenario(numTmp);
 
-			this.ctrl.lancerScenario();
+			/**lancement du scénario */
+			this.ctrl.lancerScenario();								
 		}
-		if(e.getSource() == this.btnRetour)
+		/**Cette condition sert à retourner au menu principal */
+		if(e.getSource() == this.btnRetour)							
 		{
-			this.ctrl.retour('s');
+			/**On envoie 's' à la méhode permettant de retourner au menu principal */
+			this.ctrl.retour('s');									
 		}
 	}
 
-	public void paintComponent(Graphics g)
+	/**Méhode pour dessiner le fond */
+	public void paintComponent(Graphics g)							
 	{
 		super.paintComponent(g);
 
