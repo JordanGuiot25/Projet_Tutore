79b0a473857744b000e9c7ff1a513fc6737b1816
Gaspard Gordien <gaspard.gordien@etu.univ-lehavre.fr>
2021-06-14 15:00:56 +0200

    ihm javadoc v2

--- Code/ihm/gui/PanelMenu.java
+++ Code/ihm/gui/PanelMenu.java
@@ -74,14 +74,12 @@ public class PanelMenu extends JPanel implements ActionListener
         this.imgFond              = getToolkit().getImage ( "../Ressources/fond.png" );
         this.imgPartieRapide      = getToolkit().getImage ( "../Ressources/PartieRapide.png" );
         this.imgLancerScenario    = getToolkit().getImage ( "../Ressources/LancerScenario.png" );
-        this.imgChargerSave       = getToolkit().getImage ( "../Ressources/ChargerSauvegarde.png" );
         this.imgPartieCustom      = getToolkit().getImage ( "../Ressources/PartieCustom.png" );
         this.imgQuitter           = getToolkit().getImage ( "../Ressources/Quitter.png" );
         
         /*boutons*/
         this.btnPartieRapide      = new JButton(new ImageIcon(imgPartieRapide));
         this.btnLancerScenar      = new JButton(new ImageIcon(imgLancerScenario));
-        this.btnChargerSauvegarde = new JButton(new ImageIcon(imgChargerSave));
         this.btnPartieCustom      = new JButton(new ImageIcon(imgPartieCustom));
         this.btnQuitter           = new JButton(new ImageIcon(imgQuitter));
 
@@ -100,10 +98,6 @@ public class PanelMenu extends JPanel implements ActionListener
         this.btnLancerScenar.setOpaque(false);
         this.btnLancerScenar.setContentAreaFilled(false);
         this.btnLancerScenar.setBorderPainted(false);
-
-        this.btnChargerSauvegarde.setOpaque(false);
-        this.btnChargerSauvegarde.setContentAreaFilled(false);
-        this.btnChargerSauvegarde.setBorderPainted(false);
 
         this.btnPartieCustom.setOpaque(false);
         this.btnPartieCustom.setContentAreaFilled(false);
@@ -130,7 +124,7 @@ public class PanelMenu extends JPanel implements ActionListener
 
         this.add(new JLabel(""));
         this.add(new JLabel(""));
-        this.add(new JLabel(""));//this.add(this.btnChargerSauvegarde);
+        this.add(new JLabel(""));
         this.add(new JLabel(""));
         this.add(new JLabel(""));
         this.add(this.btnQuitter);
@@ -143,7 +137,6 @@ public class PanelMenu extends JPanel implements ActionListener
 		
         this.btnPartieRapide     .addActionListener(this);
         this.btnLancerScenar     .addActionListener(this);
-        this.btnChargerSauvegarde.addActionListener(this);
         this.btnPartieCustom     .addActionListener(this);
         this.btnQuitter          .addActionListener(this);
         repaint();
