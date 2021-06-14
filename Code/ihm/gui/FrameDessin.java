1ccd978f13452c4aefcfec11e6f1e9d3f8a141cf
Enguerrand Beltran <be200919@etu.univ-lehavre.fr>
2021-06-14 08:37:18 +0200

    Ajout du JScrollPane a la frame du jeu

--- Code/ihm/gui/FrameDessin.java
+++ Code/ihm/gui/FrameDessin.java
@@ -1,6 +1,8 @@
 package PilierDeLaTerre.ihm.gui;
 
 import PilierDeLaTerre.ihm.gui.PanelDessin;
+
+import java.awt.Dimension;
 import java.awt.Point;
 
 
@@ -11,15 +13,19 @@ import javax.swing.*;
 
 import java.awt.event.*;
 
-public class FrameDessin extends JFrame
+public class FrameDessin extends JFrame implements AdjustmentListener
 {
 	private PanelDessin panel	 ;
 	private Controleur  ctrl	 ;
 	public static final double POS_X = 400;
 	public static final double POS_Y = 0;
+	private JScrollPane scrollPane;
+	private JScrollBar horizontal;
+	private JScrollBar vertical;
 
 	public FrameDessin(Controleur  ctrl)
 	{
+		Point p = new Point(400,800);
 		this.ctrl = ctrl;
 
 
@@ -31,6 +37,20 @@ public class FrameDessin extends JFrame
 		
 		this.add( this.panel );
 		this.addComponentListener( new GereDeplacerFrame());
+		this.scrollPane = new JScrollPane(this.panel, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+				this.scrollPane.setPreferredSize(new Dimension(800,500));
+
+
+
+
+				
+		this.scrollPane.getVerticalScrollBar().addAdjustmentListener(this);
+		this.scrollPane.getHorizontalScrollBar().addAdjustmentListener(this);
+		this.scrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
+		this.scrollPane.getViewport().setViewPosition(p);
+		this.add(this.scrollPane);
 
 
 		this.setVisible(false);
@@ -50,5 +70,17 @@ public class FrameDessin extends JFrame
 	public void miseAJourGrille()
 	{
 		this.panel.miseAJourGrille();
+	}
+	
+	
+	public void adjustmentValueChanged(AdjustmentEvent e) 
+	{
+		if(e.getSource().equals(this.scrollPane.getHorizontalScrollBar())|| e.getSource().equals(this.scrollPane.getVerticalScrollBar()))
+		{
+			//System.out.println(this.scrollPane.getHorizontalScrollBar());
+			panel.revalidate();
+			panel.repaint();
+
+		}
 	}
 }
--- Code/ihm/gui/PanelDessin.java
+++ Code/ihm/gui/PanelDessin.java
@@ -6,6 +6,7 @@ import PilierDeLaTerre.metier.Pilier;
 
 import javax.swing.JPanel;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -18,11 +19,15 @@ public class PanelDessin extends JPanel
 	private static final Image RESSOURCE_ANNEAU_MARON = java.awt.Toolkit.getDefaultToolkit().getImage("../Ressources/anneau_maron.png");
 	private static final Image RESSOURCE_ANNEAU_GRIS  = java.awt.Toolkit.getDefaultToolkit().getImage("../Ressources/anneau_gris.png" );
 	private Controleur  ctrl;
+	/** Taille en largeur du panel calculée en fonction de la largeur des images moins les partie qui se superposent multiplié par leur nombre*/
+	private final int RES_X = (67-16)*31;
+	/** Taille en hauteur du panel calculée en fonction de la hauteur des images multiplié par leur nombre*/
+	private final int RES_Y = (67*31)+33;
 
 	public PanelDessin(Controleur  ctrl)
 	{
 		this.ctrl  = ctrl;
-
+		this.setPreferredSize((new Dimension(this.RES_X+34,this.RES_Y)));
 		this.miseAJourGrille();
 	}
 
