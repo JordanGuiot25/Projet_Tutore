451b97d8a958847be025b1eced13744fbe223bd0
Jordan Guiot <Jordan.guiot25@gmail.com>
2021-06-16 15:55:18 +0200

    Javadoc de Joueur.java

--- Code/metier/Joueur.java
+++ Code/metier/Joueur.java
@@ -7,13 +7,37 @@ import java.util.ArrayList;
 
 public class Joueur 
 {
+    /** Liste des dalles que le joueur possède
+     * @see listeDalles {@link Arraylist<Dalle> } 
+     */
     private ArrayList<Dalle>   listeDalles;
 
+    /** Numero du joueur
+     * @see numJoueur {@link Int}
+     */
     private int  numJoueur ;
+
+    /** Couleur du joueur
+     * @see coulJoueur {@link Char}
+     */
     private char coulJoueur;
+
+    /** Nombre de pilier restant au joueur
+     * @see nbPilier {@link Int}
+     */
     private int  nbPilier;
+
+    /** Nombre de pilier détruit par le joueur
+     * @see nbPilierDetruis {@link Int}
+     */
     private int  nbPilierDetruis;
 
+
+    /**
+     * Constructeur de la class Joueur
+     * @param numJoueur
+     * @param coulJoueur
+     */
     public Joueur(int numJoueur, char coulJoueur)
     {
         this.numJoueur       = numJoueur;
@@ -23,6 +47,10 @@ public class Joueur
         this.listeDalles = new ArrayList<Dalle> ();
     }
 
+    /**
+     * Decremente le nombre de pilier restant 
+     * @return validation {@link Boolean }
+     */
     public boolean poserPilier()
     {
         if(nbPilier > 0)
@@ -34,6 +62,10 @@ public class Joueur
         return false;
     }
 
+    /**
+     * Ajoute une dalle dans l'ensemble de dalle du Joueur
+     * @param dalle {@link Dalle }
+     */
     public void ajouterDalles(Dalle dalle)
     {
         boolean estDejaAjouter = false;
@@ -46,34 +78,79 @@ public class Joueur
         if ( !estDejaAjouter )
             this.listeDalles.add(dalle);
     }
+
+    /**
+     * Retire une dalle dans la liste du joueur
+     * @param dalle {@link Dalle }
+     */
     public void retirerDalle(Dalle dalle)
     {
         this.listeDalles.remove(dalle);
     }
 
-
+    /**
+     * Rajoute un certain nombre au nombre de pilier restant
+     * @param incrementation {@link Int }
+     */
     public void incrementationNbPilier(int incrementation)
     {
         this.nbPilier += incrementation;
     }
+
+    /**
+     * Rajoute un certain nombre au nombre de pilier détruis
+     * @param incrementation {@link Int }
+     */
     public void incrementationNbPilierDetruis(int incrementation)
     {
         this.nbPilierDetruis += incrementation;
     }
 
+    /**
+     * Définie à une valeur le nombre de pilier restant
+     * @param nbPilier
+     */
     public void setNbPilier(int nbPilier)
     {
         this.nbPilier = nbPilier;
     }
+
+    /**
+     * Définie à une valeur le nombre de pilier détruit
+     * @param nbPilier
+     */
     public void setPilierDetruit(int nbPilierDetruis)
     {
         this.nbPilierDetruis = nbPilierDetruis;
     }
 
+    /**
+     * Retourne le numéro du joueur
+     * @return numJoueur {@link Int}
+     */
     public int  getNumJoueur()       { return this.numJoueur;       }
+
+    /**
+     * Retourne le nombre de pilier restant
+     * @return nbPilier {@link Int}
+     */
     public int  getNbPilier ()       { return this.nbPilier;        }
+
+    /**
+     * Retourne le nombre de pilier detruis
+     * @return nbPilierDetruis {@link Int}
+     */
     public int  getNbPilierDetruis() { return this.nbPilierDetruis; }
+
+    /**
+     * Retourne la couleur du joueur
+     * @return coulJoueur {@link Char }
+     */
     public char getCouleur()         { return this.coulJoueur;      }
 
+    /**
+     * Retourne la liste des dalles du joueur
+     * @return listeDalles {@link ArrayList<Dalle> }
+     */
     public ArrayList<Dalle>  getListeDalles() { return this.listeDalles; }
 }
